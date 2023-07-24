c2d97c5496f2825ae13ee08d9999685fd0711418
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-07-24 17:27:07 +0400

    Added styles for Edit page & added redirection

--- blog-fe/pages/EditPost.tsx
+++ blog-fe/pages/EditPost.tsx
@@ -1,9 +1,11 @@
 // EditPost.js (Edit component for editing a blog post)
 import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
 import { fetchBlogPostById, updateBlogPost } from '../pages/api/posts';
 import { BlogContext } from './contexts/BlogContext';
 
 const EditPost = ({ postId }) => {
+  const router = useRouter();
   const { blogData, setBlogData } = useContext(BlogContext);
   const [post, setPost] = useState({
     title: '',
@@ -40,15 +42,17 @@ const EditPost = ({ postId }) => {
         draft: post.draft,
       });
 
+      router.push('/');
+
       //update the blog data in the context with the edited data
       setBlogData((prevBlogData: { _id: any }[]) => {
-        const updaatedBlogData = prevBlogData.map((postItem: { _id: any }) => {
+        const updatedBlogData = prevBlogData.map((postItem: { _id: any }) => {
           if (postItem._id === postId) {
             return { ...postItem, ...post };
           }
           return postItem;
         });
-        return updaatedBlogData;
+        return updatedBlogData;
       });
 
       // Handle success, e.g., show a success message or redirect to the post page
@@ -59,34 +63,56 @@ const EditPost = ({ postId }) => {
   };
 
   return (
-    <div>
-      <h1>Edit Blog Post</h1>
-      <label>
-        Title:
+    <div className='max-w-xl mx-12 mt-12'>
+      <h1 className='text-2xl font-bold mb-4'>Edit Blog Post</h1>
+      <div className='mb-4'>
+        <label className='block mb-2' htmlFor='title'>
+          Title:
+        </label>
         <input
+          className='w-full px-4 py-2 border rounded focus:outline-none focus:border-blue-500'
           type='text'
+          id='title'
           name='title'
           value={post.title}
           onChange={handleInputChange}
         />
-      </label>
-      <label>
-        Content:
-        <textarea value={post.content} />
-      </label>
-      <label>
-        Author:
-        <input type='text' name='title' value={post.author} />
-      </label>
-
-      <label>
-        Draft:
+      </div>
+      <div className='mb-4'>
+        <label className='block mb-2' htmlFor='content'>
+          Content:
+        </label>
+        <textarea
+          className='w-full px-4 py-2 border rounded focus:outline-none focus:border-blue-500'
+          id='content'
+          value={post.content}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className='mb-4'>
+        <label className='block mb-2' htmlFor='author'>
+          Author:
+        </label>
+        <input
+          className='w-full px-4 py-2 border rounded focus:outline-none focus:border-blue-500'
+          type='text'
+          id='author'
+          name='author'
+          value={post.author}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className='mb-4'>
+        <label className='block mb-2' htmlFor='draft'>
+          Draft:
+        </label>
         <input
           type='checkbox'
+          id='draft'
           checked={post.draft}
           onChange={handleDraftChange}
         />
-      </label>
+      </div>
       <button
         className='bg-green-500 text-white px-4 py-2 rounded'
         onClick={handleSave}
