a838772308fd411f5b7eb3ab72c3e06fcf7a135e
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-08-01 00:04:16 +0400

    Updated Button types

--- blog-fe/pages/Button.tsx
+++ blog-fe/pages/Button.tsx
@@ -1,4 +1,18 @@
-export const Button = ({ icon = null, text, className, onClick }) => {
+import React, { ReactNode, MouseEventHandler } from 'react';
+
+interface ButtonProps {
+  icon?: ReactNode;
+  text: string;
+  className?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  icon,
+  text,
+  className = '',
+  onClick,
+}) => {
   return (
     <button
       className={`flex items-center mt-4 rounded-full text-black px-6 py-2 font-medium ${className}`}
